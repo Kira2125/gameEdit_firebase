9c9d9cf0e2f8353aea907175f2988dfb6c0de8c9
unknown <c.o.n.s.t.a.n.t.tnatsnoc@gmail.com>
2019-11-13 23:19:34 +0300

    Updates

---
+++ precache-manifest.544bdbe6a2e99bc5db07278ca66fa362.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ffd5d7118638e1785ece3ac74e94899",
+    "url": "/gameEdit/index.html"
+  },
+  {
+    "revision": "24e0fb05b257b055557b",
+    "url": "/gameEdit/static/css/2.f28040ae.chunk.css"
+  },
+  {
+    "revision": "992c865e2b4573543c4f",
+    "url": "/gameEdit/static/css/main.80756a12.chunk.css"
+  },
+  {
+    "revision": "24e0fb05b257b055557b",
+    "url": "/gameEdit/static/js/2.e185964f.chunk.js"
+  },
+  {
+    "revision": "992c865e2b4573543c4f",
+    "url": "/gameEdit/static/js/main.7cd22677.chunk.js"
+  },
+  {
+    "revision": "8ff5f25a0039cfa8595f",
+    "url": "/gameEdit/static/js/runtime-main.73cb52ca.js"
+  },
+  {
+    "revision": "365b356e04474a4a07c3134be2a036ff",
+    "url": "/gameEdit/static/media/l6.365b356e.jpg"
+  },
+  {
+    "revision": "1c559bdb6ed52e5c96a50fb083e8dc7c",
+    "url": "/gameEdit/static/media/new_logo.1c559bdb.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/gameEdit/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/gameEdit/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/gameEdit/static/media/revicons.2feb69cc.eot"
+  }
+]);
