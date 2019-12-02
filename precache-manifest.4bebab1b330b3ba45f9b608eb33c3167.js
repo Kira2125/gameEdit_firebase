ed307b06fe1877ff0c283dffbee17700e89691d3
unknown <c.o.n.s.t.a.n.t.tnatsnoc@gmail.com>
2019-12-02 22:33:01 +0300

    Updates

---
+++ precache-manifest.4bebab1b330b3ba45f9b608eb33c3167.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef9863f389892fde47a16841a2beded",
+    "url": "/gameEdit_firebase/index.html"
+  },
+  {
+    "revision": "16650cdb76b968dd9421",
+    "url": "/gameEdit_firebase/static/css/2.ed31c631.chunk.css"
+  },
+  {
+    "revision": "a2c5a1d22e3f1d7c18f6",
+    "url": "/gameEdit_firebase/static/css/main.f7b89b0b.chunk.css"
+  },
+  {
+    "revision": "16650cdb76b968dd9421",
+    "url": "/gameEdit_firebase/static/js/2.32ec65c3.chunk.js"
+  },
+  {
+    "revision": "a2c5a1d22e3f1d7c18f6",
+    "url": "/gameEdit_firebase/static/js/main.82427658.chunk.js"
+  },
+  {
+    "revision": "5aa573c15c99fc517227",
+    "url": "/gameEdit_firebase/static/js/runtime-main.70c2c079.js"
+  },
+  {
+    "revision": "1c559bdb6ed52e5c96a50fb083e8dc7c",
+    "url": "/gameEdit_firebase/static/media/new_logo.1c559bdb.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/gameEdit_firebase/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/gameEdit_firebase/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/gameEdit_firebase/static/media/revicons.2feb69cc.eot"
+  }
+]);
